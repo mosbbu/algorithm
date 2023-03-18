9a35518aadeb7f492e4cb20309fda8619fb758f2
sukyeongs <sukyeongseo@ewhain.net>
2023-03-18 22:45:27 +0900

    [E-PPER] BOJ - 11399 ATM

--- sukyeongs/EPPER/11399.py
+++ sukyeongs/EPPER/11399.py
@@ -1,1 +1,12 @@
 # BOJ - 11399 ATM
+# BOJ - 11399 ATM
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+time = list(map(int, input().split()))
+time.sort()
+for i in range(1, n):
+    time[i] = time[i] + time[i-1]
+
+print(sum(time))
